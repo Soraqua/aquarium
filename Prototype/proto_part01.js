8949966612bf90af4727c0f699a19739ac208116
soraqua <movie1290@gmail.com>
2024-04-29 08:27:21 -0500

    push

---
+++ Prototype/proto_part01.js
@@ -0,0 +1,16 @@
+/*
+I am creating a tic tac toe game.
+These are the steps I need to do:
+Create background
+Create lines for grid
+Create an O object/class
+maybe use array to
+breakdown into 3 x 3 board
+Create an X object/class
+breakdown into 3 x 3 board
+Mouse click to make X and O appear
+If/Then for rows, col and diag then Win
+Create Win and Reset Button
+If/Then no winners 
+No Win and Reset Button
+*/
